af4ad8cedd2485693184b5492172c907f9d72743
gendronx <gendronx@educ.college-montreal.qc.ca>
2024-11-29 14:55:05 -0500

    initial commit

--- 4.3.py
+++ 4.3.py
@@ -32,6 +32,44 @@ class NpcStats:
         self.cha: int = randint(3, 18)
 
 
+@dataclass
+class Item:
+    quantite: int
+    name: str
+
+
+class Inventory:
+    def __init__(self):
+        self.list_item = []
+        self.name_item = Item.name
+        self.quantite_item = Item.quantite
+
+    def ajouter_item(self, item):
+        """
+        si item présent, additionner la quantité
+        sinon si item pas présent, ajouter automatiqurement
+        """
+        self.list_item.index(self.name_item)
+        if self.name_item in self.list_item:
+            self.list_item.append(item)
+
+        elif self.name_item not in self.list_item:
+            self.list_item.append(item)
+
+    def retirer_item(self, item):
+        self.list_item.remove(item)
+
+    def voir_contenu(self):
+        print(self.list_item)
+
+
+inv = Inventory()
+
+inv.ajouter_item(Item(10, "Or"))
+inv.ajouter_item(Item(10, "Or"))
+print(inv.list_item)
+
+
 class Npc:
     def __init__(self):
         self.npc_stats = NpcStats()
@@ -41,6 +79,7 @@ class Npc:
         self.race = ""
         self.species = ""
         self.profession = ""
+        self.alignement = "Non defini"
 
     def afficher_npc(self):
         print(f"Nom: {self.name}"
@@ -54,7 +93,19 @@ class Npc:
               f" Constitution: {self.npc_stats.con}"
               f" Intelligence: {self.npc_stats.intelligence}"
               f" Sagesse: {self.npc_stats.wis}"
-              f" Charisme: {self.npc_stats.cha}")
+              f" Charisme: {self.npc_stats.cha}"
+              f" Alignement: {self.alignement}")
+
+    def alive(self):
+        if self.hp > 0:
+            pass
+
+        else:
+            print("Vous etes mort")
+            exit()
+
+    def attaquer(self):
+        degat_roll = 0
 
 
 class Kobold(Npc):
@@ -68,7 +119,7 @@ class Kobold(Npc):
         self.hp = self.hp - degat
 
 
-class Hero(Npc):
+class Hero(Npc, Inventory):
 
     def __init__(self):
         super().__init__()
